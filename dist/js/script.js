03b83fc49ebab92886b266926d0c742254ccb88a
Joshua Wynns <josh1132003@gmail.com>
2018-12-22 12:07:33 -0500

    Added menu toggle, classList bug

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,34 @@
 //  Select Dom Items
 
-const menuBtn = document.querySelector('.btn-menu');
+const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.btn-nav');
 const menuBranding = document.querySelector('.btn-branding');
-const navItems = document.querySelectorAll('.nav-items;');
+const navItem = document.querySelectorAll('.nav-item');
 
-// set Initial State Of Menu
+// Set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+	if (!showMenu) {
+		menuBtn.classList.add('close');
+		menu.classList.add('show');
+		menuNav.classList.add('show');
+		menuBranding.classList.add('show');
+		navItems.forEach(items => items.classList.add('show'));
+
+		// Set Menu State
+		showMenu = true;
+	} else {
+		menuBtn.classList.remove('close');
+		menu.classList.remove('show');
+		menuNav.classList.remove('show');
+		menuBranding.classList.remove('show');
+		navItems.forEach(items => items.classList.remove('show'));
+
+		// Set Menu State
+		showMenu = false;
+	}
+}
